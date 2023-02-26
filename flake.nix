7058477a40f8ac43667620e8d16b02459a5876fe
Christian Albrecht <christian.albrecht@mayflower.de>
2023-02-26 19:38:14 +0100

    add starttls to goimapnotify

--- flake.nix
+++ flake.nix
@@ -72,6 +72,26 @@ index e2e1def5..f2bc621f 100644
           '';
       });
 
+      goimapnotify = prev.goimapnotify.overrideAttrs (old: {
+        postPatch = (old.postPatch or "") + ''
+            substituteInPlace client.go \
+            --replace 'client.Dial(fmt.Sprintf("%s:%d", conf.Host, conf.Port))' \
+              'client.Dial(fmt.Sprintf("%s:%d", conf.Host, conf.Port))
+                      err = c.StartTLS(&tls.Config{
+                              ServerName:         conf.Host,
+                              InsecureSkipVerify: !conf.TLSOptions.RejectUnauthorized,
+                      })'
+
+            substituteInPlace client.go \
+            --replace 'client.Dial(conf.Host + fmt.Sprintf(":%d", conf.Port))' \
+              'client.Dial(conf.Host + fmt.Sprintf(":%d", conf.Port))
+                      err = c.StartTLS(&tls.Config{
+                              ServerName:         conf.Host,
+                              InsecureSkipVerify: !conf.TLSOptions.RejectUnauthorized,
+                      })'
+        '';
+      });
+
       fmt_8 = prev.fmt_8.overrideAttrs (old: rec {
         version = "8.1.1";
         src = prev.fetchFromGitHub {
